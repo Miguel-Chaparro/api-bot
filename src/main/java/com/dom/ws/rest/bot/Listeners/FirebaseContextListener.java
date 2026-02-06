b922608261062920d8d55d939e37f5ab5c88f1df
Miguel-Chaparro <miguel.chapariza@gmail.com>
2026-02-06 08:20:16 -0500

    fix listener

--- src/main/java/com/dom/ws/rest/bot/Listeners/FirebaseContextListener.java
+++ src/main/java/com/dom/ws/rest/bot/Listeners/FirebaseContextListener.java
@@ -5,6 +5,11 @@ import com.google.firebase.FirebaseApp;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.logging.Logger;
 
 @WebListener
@@ -14,7 +19,7 @@ public class FirebaseContextListener implements ServletContextListener {
     
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        log.info("Inicializando contexto de Firebase...");
+        log.info("=== Inicializando contexto de la aplicación ===");
         try {
             FirebaseInitializer.getInstance().initialize();
             log.info("Firebase inicializado correctamente");
@@ -26,9 +31,11 @@ public class FirebaseContextListener implements ServletContextListener {
     
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        log.info("Destruyendo contexto de Firebase...");
+        log.info("=== Destruyendo contexto de la aplicación ===");
+        
+        // Limpiar FirebaseApp
         try {
-            // Cerrar todas las instancias de FirebaseApp
+            log.info("Limpiando instancias de FirebaseApp...");
             for (FirebaseApp app : FirebaseApp.getApps()) {
                 app.delete();
                 log.info("FirebaseApp deletado: " + app.getName());
@@ -38,5 +45,25 @@ public class FirebaseContextListener implements ServletContextListener {
             log.severe("Error al limpiar recursos de Firebase: " + e.getMessage());
             e.printStackTrace();
         }
+        
+        // Limpiar JDBC Drivers
+        try {
+            log.info("Limpiando JDBC Drivers...");
+            Enumeration<Driver> drivers = DriverManager.getDrivers();
+            while (drivers.hasMoreElements()) {
+                Driver driver = drivers.nextElement();
+                try {
+                    DriverManager.deregisterDriver(driver);
+                    log.info("JDBC Driver deregistrado: " + driver.getClass().getName());
+                } catch (SQLException e) {
+                    log.warning("Error deregistrando driver " + driver.getClass().getName() + ": " + e.getMessage());
+                }
+            }
+        } catch (Exception e) {
+            log.severe("Error limpiando JDBC Drivers: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
+        log.info("=== Contexto destruido correctamente ===");
     }
 }
